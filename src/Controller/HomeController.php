e2581fe59ae4eac58b96243d303b59e69443322b
Woeler <woeler@woeler.eu>
2019-12-08 18:42:41 +0100

    [TASK] Add fluff texts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,25 +9,44 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      * @param EventRepository $eventRepository
+     * @param UrlGeneratorInterface $router
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function home(EventRepository $eventRepository)
+    public function home(EventRepository $eventRepository, UrlGeneratorInterface $router)
     {
         if (!$this->isGranted('ROLE_USER')) {
             return $this->render('home.html.twig');
         }
 
-        if ('UTC' === $this->getUser()->getTimezone()) {
-            $this->addFlash('warning', 'Your current timezone is set to UTC, please configure your home timezone in <a href="/user/update">your user settings</a>.');
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ('UTC' === $user->getTimezone()) {
+            $this->addFlash(
+                'warning',
+                'Your current timezone is set to UTC, please configure your home timezone in <a href="'
+                .$router->generate('user_update')
+                .'">your user settings</a>.'
+            );
+        }
+        if (0 < $user->getDiscordGuilds()->count() && 0 === $user->getActiveDiscordGuilds()->count()) {
+            $this->addFlash(
+                'info',
+                'You are not in any active guilds, but you do own Discord servers. You can start the setup process for your Discord servers <a href="'
+                .$router->generate('user_guilds')
+                .'">here</a>.'
+            );
         }
 
         return $this->render(
