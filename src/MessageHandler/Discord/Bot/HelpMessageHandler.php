9b6e3be7833510b96d2d17bc59a42dab40ee76f9
Woeler <woeler@woeler.eu>
2020-02-29 21:38:05 +0100

    [TASK] Add timezones to help

--- src/MessageHandler/Discord/Bot/HelpMessageHandler.php
+++ src/MessageHandler/Discord/Bot/HelpMessageHandler.php
@@ -44,6 +44,8 @@ class HelpMessageHandler implements MessageHandlerInterface
             ->addField('Attend event', '!attend [eventId] [class] [role]'.PHP_EOL.'**Example**: `!attend 1 dragonknight tank`')
             ->addField('Leave event', '!unattend [eventId]'.PHP_EOL.'**Example**: `!unattend 1`')
             ->addField('See your character presets', '`!characters`')
+            ->addField('Change your preferred timezone', '!timezone [timezone]')
+            ->addField('List of valid timezones', '[Click here](https://www.php.net/manual/en/timezones.php)')
             ->addField('Usable classes', implode(', ', EsoClassUtility::toArray()))
             ->addField('Usable roles', implode(', ', EsoRoleUtility::toArray()));
         $discordMessage->setContent($user->getDiscordMention());
